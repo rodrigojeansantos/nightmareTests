90f89cf16071755b2a712d8fba1a195120fbddd5
Rodrigo Jean <rodrigojeansantos@gmail.com>
2019-12-01 20:54:26 -0300

    Send files

---
+++ js4t/main.js
@@ -0,0 +1,17 @@
+let welcome = 'Hello Javascript - Automação Full Stack from QA Ninja '
+    document.getElementById('welcome').append(welcome)
+    console.log(welcome)
+
+    console.log(typeof welcome)
+
+/*
+
+  html => Pagina
+
+  h1 = Cabeçalho/ Titulos
+  p  = Parágrafos
+  ul = Agrupadores de Listas
+  li = Os itens de listas do elemento ul
+  a = Link
+
+*/
